581d8fe880cd7a7ec63d43ba51397f72deb378dd
rodrigo-montejr <rodrigo.montejr@ibm.com>
2022-05-06 16:33:07 -0300

    Registros paginados.

--- rest-api-main/src/main/java/com/ibm/restapibancocentral/wrapper/Pageable.java
+++ rest-api-main/src/main/java/com/ibm/restapibancocentral/wrapper/Pageable.java
@@ -12,23 +12,23 @@ import org.springframework.data.domain.PageRequest;
 
 @Getter
 @Setter
-public class PageableResponse<T> extends PageImpl<T> {
+public class Pageable<T> extends PageImpl<T> {
     private boolean first;
     private boolean last;
     private int totalPages;
     private int numberOfElements;
 
     @JsonCreator(mode = Mode.PROPERTIES )
-    public PageableResponse(@JsonProperty("content") List<T> content,
-                            @JsonProperty("number") int number,
-                            @JsonProperty("size") int size,
-                            @JsonProperty("totalElements") int totalElements,
-                            @JsonProperty("last") boolean last,
-                            @JsonProperty("first") boolean first,
-                            @JsonProperty("totalPages") int totalPages,
-                            @JsonProperty("numberOfElements") int numberOfElements,
-                            @JsonProperty("pageable") JsonNode pageable,
-                            @JsonProperty("sort") JsonNode sort) {
+    public Pageable(@JsonProperty("content") List<T> content,
+                    @JsonProperty("number") int number,
+                    @JsonProperty("size") int size,
+                    @JsonProperty("totalElements") int totalElements,
+                    @JsonProperty("last") boolean last,
+                    @JsonProperty("first") boolean first,
+                    @JsonProperty("totalPages") int totalPages,
+                    @JsonProperty("numberOfElements") int numberOfElements,
+                    @JsonProperty("pageable") JsonNode pageable,
+                    @JsonProperty("sort") JsonNode sort) {
         super(content, PageRequest.of(number, size), totalElements);
 
         this.last = last;
