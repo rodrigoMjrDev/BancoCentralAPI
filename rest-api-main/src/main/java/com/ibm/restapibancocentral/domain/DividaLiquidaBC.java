913b5b90ae8c9e46ebe87c66e3eff80acf14a9be
rodrigo-montejr <rodrigo.montejr@ibm.com>
2022-05-06 09:54:53 -0300

    Declaração das variáveis na classe domain.

--- rest-api-main/src/main/java/com/ibm/restapibancocentral/domain/DividaLiquidaBC.java
+++ rest-api-main/src/main/java/com/ibm/restapibancocentral/domain/DividaLiquidaBC.java
@@ -20,6 +20,7 @@ import java.util.Date;
 @Builder
 public class DividaLiquidaBC {
 
+    //Declaração das variáveis na classe domain.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
